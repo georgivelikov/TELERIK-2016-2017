0c243664db8ea8d3783776825e98590f5c33d38e
georgivelikov <gmvelikov@gmail.com>
2017-03-06 19:43:15 +0200

    Start

---
+++ Module III/19. ASP MVC/Fancy/Fancy.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fancy.Web.MvcApplication" Language="C#" %>
---
+++ Module III/19. ASP MVC/Fancy/Fancy.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fancy.Web.Startup))]
+namespace Fancy.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
